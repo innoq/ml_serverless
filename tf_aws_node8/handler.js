fe8c84aadd9c48f07a5ca2e5ad770521b5d6543b
Leonardo Ramirez <leonardo.ramirez@gmail.com>
2018-12-21 15:26:53 +0100

    Fixes aws lambda version of image recognition

--- tf_aws_node8/handler.js
+++ tf_aws_node8/handler.js
@@ -3,15 +3,16 @@ const os = require('os');
 const fs = require('fs');
 const jpeg = require('jpeg-js');
 const Busboy = require('busboy');
-global.fetch = require('node-fetch')
+const tf = require('@tensorflow/tfjs');
+require('@tensorflow/tfjs-converter');
+require('@tensorflow/tfjs-node');
+const shortid = require('shortid');
+global.fetch = require('node-fetch');
 
-const tf = require('@tensorflow/tfjs')
-const mobilenet = require('@tensorflow-models/mobilenet');
-require('@tensorflow/tfjs-converter')
-require('@tensorflow/tfjs-node')
+const tfc = require('@tensorflow/tfjs-core');
 
-const MODEL_URL = 'https://storage.googleapis.com/ml-flowers/tensorflowjs_model.pb';
-const WEIGHTS_URL = 'https://storage.googleapis.com/ml-flowers/weights_manifest.json';
+const MODEL_URL = 'https://storage.googleapis.com/which_flower/tensorflowjs_model.pb';
+const WEIGHTS_URL = 'https://storage.googleapis.com/which_flower/weights_manifest.json';
 const CLASSES = ['daisy', 'dandelion', 'rose', 'sunflower', 'tulip'];
 const NUMBER_OF_CHANNELS = 3
 
@@ -25,111 +26,120 @@ const imageByteArray = (image, numChannels) => {
       values[i * numChannels + channel] = pixels[i * 4 + channel] / 255;
     }
   }
-  return values
+  return values;
 }
 
 const imageToInput = (image, numChannels) => {
-  const values = imageByteArray(image, numChannels)
+  const values = imageByteArray(image, numChannels);
   const outShape = [image.height, image.width, numChannels];
   const input = tf.tensor3d(values, outShape, 'float32').expandDims(0);
-  return input
+  return input;
 }
 
-var model = null
-
-const calc = (file) => {
-  return new Promise(async resolve => {
-    var jpegData = fs.readFileSync(file);
-    var pixels = jpeg.decode(jpegData);
-    const input = imageToInput(pixels, NUMBER_OF_CHANNELS)
-
-    if (model === null) {
-      model = new tf.FrozenModel(MODEL_URL, WEIGHTS_URL)
-      console.log('model loading...', Date.now())
-      await model.load()
-      console.log('model loaded !!!', Date.now())
-    }
-
-    console.log('classifying...', Date.now())
-    predictions = await model.predict(input)
-    predictions_array = Array.from(predictions.flatten().dataSync())
-
-    const flower = CLASSES[predictions_array.indexOf(Math.max(...predictions_array))];
-    console.log('classified!', flower)
-    resolve(flower);
-  });
+const loadModel = async () => {
+  console.log('model loading...', new Date().toISOString());
+  const model = new tf.FrozenModel(MODEL_URL, WEIGHTS_URL);
+  await model.load();
+  console.log('model loaded!', new Date().toISOString());
+  return model;
 }
 
-const getContentType = (event) => {
-  let contentType = event.headers['content-type']
-  if (!contentType) {
-    return event.headers['Content-Type'];
-  }
-  return contentType;
-};
+const calc = async (file, tfModel) => {
+  console.log("Decoding:", file);
+  var jpegData = fs.readFileSync(file);
+  var pixels = jpeg.decode(jpegData);
+  const input = imageToInput(pixels, NUMBER_OF_CHANNELS);
+  console.log('classifying...', new Date().toISOString());
+  predictions = await tfModel.predict(input);
+  predictions_array = Array.from(predictions.flatten().dataSync());
+  const flower = CLASSES[predictions_array.indexOf(Math.max(...predictions_array))];
+  console.log("predicted:", flower);
+  return flower;
+}
 
+let model;
+const parseRequest = (event) => new Promise((resolve, reject) => {
+  console.log("got a request");
 
-const headers = {
-  'Content-Type': 'application/json',
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': 'OPTIONS, POST',
-  'Access-Control-Allow-Headers': 'Content-Type'
-};
-
-
-const parseForm = (body, headers) => new Promise((resolve, reject) => {
-  const contentType = headers['Content-Type'] || headers['content-type'];
-  const bb = new Busboy({ headers: { 'content-type': contentType }});
+  const busboy = new Busboy({ headers: event.headers });
   const tmpdir = os.tmpdir();
-  //const tmpdir = '/Users/mperlin/infrastructure/ml_serverless/tmp/';
-
-  var data = {};
-
   const uploads = {};
+  const filenames = {};
   let fileWrites = [];
 
-  bb.on('file', function (fieldname, file, filename, encoding, mimetype) {
-    console.log('File [%s]: filename=%j; encoding=%j; mimetype=%j', fieldname, filename, encoding, mimetype);
-    const filepath = path.join(tmpdir, filename);
+  busboy.on('field', function(fieldname, val) {
+    const https = require("https");
+    console.log(`fetching ${val}`);
+    const filepath = path.join(tmpdir, shortid.generate());
     uploads[fieldname] = filepath;
-    const writeStream = fs.createWriteStream(filepath);
-    file.pipe(writeStream);
-
-    file.on('data', data => console.log('File [%s] got %d bytes', fieldname, data.length))
-    //.on('end', () => console.log('File [%s] Finished', fieldname));
-    file.on('end', () => {
-      writeStream.end();
-      console.log('File [%s] Finished', fieldname)
+    filenames[fieldname] = val;
+    const file = fs.createWriteStream(filepath);
+    const promise = new Promise(function(resolve, reject) {
+      var req = https.get(val, response => {
+          var stream = response.pipe(file);
+          stream.on("finish", resolve);
+          stream.on('error', reject);
+      });
+      req.on('error', function(err) {
+        reject(err);
+      });
+      req.end();
     });
-  }).on('field', (fieldname, val) => {
-    data[fieldname] = val;
-  }).on('finish', () => {
-    for (const name in uploads) {
-      file = uploads[name];
-      console.log(file)
-      resolve(file)
-      //calc(file)
-      //fs.unlinkSync(file);
-    }
-    //resolve(data);
-  }).on('error', err => {
-    reject(err);
+    fileWrites.push(promise);
   });
 
-  bb.end(body);
+  busboy.on('file', (fieldname, file, filename) => {
+    console.log(`Processing file ${filename}`);
+    const filepath = path.join(tmpdir, shortid.generate());
+    uploads[fieldname] = filepath;
+    filenames[fieldname] = filename;
+    const writeStream = fs.createWriteStream(filepath);
+    file.pipe(writeStream);
+    const promise = new Promise((resolve, reject) => {
+      file.on('end', () => {
+        writeStream.end();
+      });
+      writeStream.on('finish', resolve);
+      writeStream.on('error', reject);
+    });
+    fileWrites.push(promise);
+  });
+
+  busboy.on('finish', () => {
+    Promise.all(fileWrites)
+    .then(() => Promise.all(
+      Object
+      .keys(uploads)
+      .map(fieldname => calc(uploads[fieldname], model).then((result) => {
+        try {
+          fs.unlinkSync(uploads[fieldname]);
+        } catch (err) {};
+        return { 
+            fieldname,
+            filename: filenames[fieldname],
+            result
+        };
+      }))
+    )).then((results) => {
+      resolve(results);
+    })
+    .catch( (error) => {
+      console.log(error);
+      reject(error);
+    });
+  });
+  if (event.body) { busboy.write(event.body, event.isBase64Encoded ? 'base64' : 'binary');}
+  busboy.end();
+  /** */
 });
 
-
 exports.whichflower = async (event, context) => {
-  parseForm(event.body, event.headers).then(file => calc(file))
-
+  console.log("starting");
+  model = model || await loadModel();
+  const result = await parseRequest(event);
+  console.log("result", result);
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: 'xx'
-    })
+    body: JSON.stringify(result)
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
