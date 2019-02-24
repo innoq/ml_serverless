fc9621b3fbfd145544dffe38341140942c9402c2
michel <michael.kraemer@innoq.com>
2019-02-25 00:33:29 +0100

    working version of spacy sentiment

---
+++ spacy_sentiment_gcloud_python/main.py
@@ -0,0 +1,23 @@
+import spacy
+import json
+
+nlp = spacy.load('de_cat_100d_100k')
+
+def spacy_sentiment(request):
+    request_json = request.get_json()
+    if request.args and 'message' in request.args:
+        text = request.args.get('message')
+    elif request_json and 'message' in request_json:
+        text = request.get_json()['message']
+    else:
+        text = "Warum sind die Geier so gierig?"
+    
+    return encode_result(text, processText(text))
+
+def encode_result(text, result, encoding='json'):
+    return json.dumps([{'text': text, 'offense': result}])
+
+def processText(mytext):
+    global nlp
+    doc = nlp(mytext)
+    return doc.cats['OFFENSE']
